ab5341280b31727cdb3bf211da2115807a6fad66
hang <thinkshihang@gmail.com>
2016-01-25 16:42:54 -0500

    Implemented: find the minimum value in a sorted, rotated array

---
+++ src/array/FindMiniumValue.java
@@ -0,0 +1,36 @@
+package array;
+
+/**
+ * Find the minimum value in an rotated array
+ * The array is originally sorted.
+ * @author Hang
+ *
+ * time complexity O(logN) space complexity O(1)
+ */
+
+public class FindMiniumValue
+{
+	static int searchMin(int[] src, int left, int right)
+	{
+		if(src[right] > src[left]) return src[left];
+		
+		int mid = (right + left) / 2;
+		if(right == left + 1) return src[right];
+		
+		if(src[mid]> src[left])
+		{
+			return searchMin(src, mid, right);
+		}
+		else {
+			return searchMin(src, left, mid);
+		}
+		
+	}
+	
+	public static void main(String[] args)
+	{
+//		int[] a = {3, 4, 5, 6, 1, 2};
+		int[] a = {3, 4, 5, 6, 7, 8, 1, 2};
+		System.out.println(searchMin(a, 0, a.length - 1));
+	}
+}
